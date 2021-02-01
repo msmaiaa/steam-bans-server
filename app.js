2e33404147910ad1c261c0b3b97c3d3aaafcb07d
msmaiaa <roxx77344@gmail.com>
2021-02-01 06:19:03 -0300

    fix port issue

--- app.js
+++ app.js
@@ -53,4 +53,4 @@ require("./src/config/steam")(app);
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 .then(() => console.log('Connected to MongoDB'))
 
-app.listen(PORT, 'localhost', () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server listening on port ${PORT}`));
